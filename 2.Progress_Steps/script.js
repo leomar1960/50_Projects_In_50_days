843efe3f39ffc59e5924847242053e7476fc3a0e
leomaratsis <maratsis.leo@gmail.com>
2021-02-04 22:54:14 +0000

    update

--- 2.Progress_Steps/script.js
+++ 2.Progress_Steps/script.js
@@ -1,15 +1,50 @@
-const panels = document.querySelectorAll('.panel');
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circle = document.querySelectorAll('.circle');
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add("active");
-    })
+let currentActive = 1;
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+
 })
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove("active");
 
+next.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circle.length) {
+        currentActive = circle.length;
+    }
+    update();
+
+
+})
+
+
+function update() {
+    circle.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
     })
-};
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = (actives.length - 1) / (circle.length - 1) * 100 + '%';
+
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === 4) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
