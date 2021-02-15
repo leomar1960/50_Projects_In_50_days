52577c48c4362f1883b68fda09ca6ad42d06c791
leomaratsis <maratsis.leo@gmail.com>
2021-02-15 17:47:37 +0000

    u

---
+++ 15.Increment_Counter/script.js
@@ -0,0 +1,26 @@
+const counters = document.querySelectorAll('.counter');
+
+
+counters.forEach(counter => {
+    counter.innerText = "0";
+
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText
+        console.log(c)
+        // console.log(typeof target, target)
+
+        const increment = target / 100
+
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target;
+        }
+    }
+
+    updateCounter()
+})
